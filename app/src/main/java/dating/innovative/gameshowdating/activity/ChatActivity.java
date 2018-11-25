c8041ab0d63676bc29c6eff4a18544b74a81cde8
Triah <nicolaihedegaardjensen93@gmail.com>
2018-11-25 18:15:29 +0100

    setup of chat with messages, missing database table for messaging should be easymode to set up though

--- app/src/main/java/dating/innovative/gameshowdating/activity/ChatActivity.java
+++ app/src/main/java/dating/innovative/gameshowdating/activity/ChatActivity.java
@@ -9,6 +9,8 @@ import android.widget.EditText;
 import dating.innovative.gameshowdating.R;
 import dating.innovative.gameshowdating.util.ChatAdapter;
 
+import java.util.ArrayList;
+
 public class ChatActivity extends Activity {
 
     RecyclerView messagesView;
@@ -22,10 +24,15 @@ public class ChatActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
 
+        ArrayList<String> messages = new ArrayList<>();
+        for(int i = 0; i<3; i++){
+            messages.add("This is a test message");
+        }
+
         messagesView = (RecyclerView) findViewById(R.id.chatRecyclerView);
         messagesLayoutManager = new LinearLayoutManager(this);
         messagesView.setLayoutManager(messagesLayoutManager);
-        messagesAdapter = new ChatAdapter();
+        messagesAdapter = new ChatAdapter(messages);
         messagesView.setAdapter(messagesAdapter);
 
         writeMessageEditText = (EditText) findViewById(R.id.chatEditText);
--- app/src/main/java/dating/innovative/gameshowdating/util/ChatAdapter.java
+++ app/src/main/java/dating/innovative/gameshowdating/util/ChatAdapter.java
@@ -1,33 +1,58 @@
 package dating.innovative.gameshowdating.util;
 
+import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import dating.innovative.gameshowdating.R;
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
 
 public class ChatAdapter extends RecyclerView.Adapter<ChatAdapter.ChatViewHolder> {
 
+    private Context context;
+    private ArrayList<String> messages;
+
+    public ChatAdapter(ArrayList<String> messages){
+        this.messages = messages;
+    }
 
     @NonNull
     @Override
     public ChatViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        context = parent.getContext();
+
+        LayoutInflater layoutInflater = LayoutInflater.from(context);
+
+        View messageView = layoutInflater.inflate(R.layout.chat_message_view, parent, false);
+        ChatViewHolder chatViewHolder = new ChatViewHolder(messageView);
+        return chatViewHolder;
     }
 
     @Override
     public void onBindViewHolder(@NonNull ChatViewHolder holder, int position) {
-
+        String message = messages.get(position);
+        TextView messageTextView = holder.message;
+        messageTextView.setText(message);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return messages.size();
     }
 
     public static class ChatViewHolder extends RecyclerView.ViewHolder{
 
+        public TextView message;
+
         public ChatViewHolder(View itemView) {
             super(itemView);
+
+            this.message = itemView.findViewById(R.id.chatMessage);
         }
     }
 }
