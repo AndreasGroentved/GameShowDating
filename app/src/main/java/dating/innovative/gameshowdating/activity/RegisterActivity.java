5e4a6d6a97076a9e9d0a436562da0827da1c12ea
Triah <nicolaihedegaardjensen93@gmail.com>
2018-11-28 21:14:35 +0100

    moved register with websocket to own method to prevent some errors

--- app/src/main/java/dating/innovative/gameshowdating/activity/RegisterActivity.java
+++ app/src/main/java/dating/innovative/gameshowdating/activity/RegisterActivity.java
@@ -65,8 +65,10 @@ public class RegisterActivity extends BaseActivity {
                         User newUser = null;
                         if(radioRegisterButtonFemale.isChecked()){
                             newUser = createNewUser(registerUsername.getText().toString(), registerPassword.getText().toString(), radioRegisterButtonFemale.getText().toString(),  Integer.parseInt(registerAge.getText().toString()));
+                            createRemoteUser(newUser);
                         } else if(radioRegisterButtonMale.isChecked()){
                             newUser = createNewUser(registerUsername.getText().toString(),registerPassword.getText().toString(),radioRegisterButtonMale.getText().toString(), Integer.parseInt(registerAge.getText().toString()));
+                            createRemoteUser(newUser);
                         } else {
                             errorLabel.setText("A sex must be chosen");
                         }
@@ -123,7 +125,10 @@ public class RegisterActivity extends BaseActivity {
         user.setVideo1("");
         user.setVideo2("");
         user.setVideo3("");
+        return user;
+    }
 
+    public void createRemoteUser(User user){
         LoginActivity.ws.createUser(user, new Function1<Boolean, Unit>() {
             @Override
             public Unit invoke(Boolean aBoolean) {
@@ -131,7 +136,5 @@ public class RegisterActivity extends BaseActivity {
                 return null;
             }
         });
-
-        return user;
     }
 }
