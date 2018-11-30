a882760a656425f5d4907a44dbe79c01fdf2927f
Triah <nicolaihedegaardjensen93@gmail.com>
2018-11-30 13:17:57 +0100

    fixing pull

--- app/src/main/java/dating/innovative/gameshowdating/activity/MainActivity.kt
+++ app/src/main/java/dating/innovative/gameshowdating/activity/MainActivity.kt
@@ -1,22 +1,30 @@
-package dating.innovative.gameshowdating
+package dating.innovative.gameshowdating.activity
 
+import android.content.Intent
 import android.os.Bundle
-import android.support.design.widget.Snackbar
-import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar
 import android.view.Menu
 import android.view.MenuItem
-
+import dating.innovative.gameshowdating.R
+import dating.innovative.gameshowdating.util.BaseActivity
 import kotlinx.android.synthetic.main.activity_main.*
-class MainActivity : AppCompatActivity() {
+import kotlinx.android.synthetic.main.content_main.*
+
+
+class MainActivity : BaseActivity() {
+    override fun getLayout(): Int = R.layout.activity_main
+    override fun getToolBar(): Toolbar = toolbar
+
+    override fun setUpToolBar() = setSupportActionBar(toolbar)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+
+        switchScreen.setOnClickListener {
+            println("switch")
+            val intent = Intent(applicationContext, LoginActivity::class.java)
+            startActivity(intent)
         }
     }
 
@@ -30,7 +38,7 @@ class MainActivity : AppCompatActivity() {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        return when(item.itemId) {
+        return when (item.itemId) {
             R.id.action_settings -> true
             else -> super.onOptionsItemSelected(item)
         }
